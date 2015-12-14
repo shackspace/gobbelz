e975752b673da4fa5dea165ac92bf98acbb7e1f8
root <root@lounge.kiosk>
2015-12-14 23:23:58 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Gobbelz
+	
+	virtualenv -p python3 .
+	. bin/activate
+	pip install -r DEPS
+	python3 routes.py
+
+
--- routes.py
+++ routes.py
@@ -1,9 +1,12 @@
-#! /usr/bin/python3
+#! /usr/bin/env python2
 # -*- coding: utf-8 -*-
 
 from flask import Flask, abort, request, redirect, jsonify
-import simplejson
+import json 
 import os
+import subprocess
+from urllib.request import urlopen
+from mpd import MPDClient
 
 app = Flask(__name__)
 app.config.update(Debug=True)
@@ -11,16 +14,79 @@ app.config.update(Debug=True)
 @app.route('/')
 def home():
     return("Welcome to gobbelz, say something!")
+
+@app.route('/mpd/status')
+def mpd_status():
+    client = MPDClient()
+    client.timeout = 10
+    client.idletimeout = None
+    client.connect("mpd.shack", 6600)
+    answer = client.currentsong()
+    state = client.status()
+    client.close()
+    client.disconnect()
+    if 'artist' in answer:
+        return jsonify(artist = answer['artist'],
+                   title = answer['title'],
+                   status = state['state'],
+                   stream = 'false')
+    elif 'name' in answer:
+        return jsonify(name=answer['name'],
+                       title=answer['title'],
+                       stream='true',
+                       status = state['state'])
+    elif 'file' in answer:
+        return jsonify(title=answer['file'],
+                       status = state['state'],
+                       stream='undef')
+    else:
+        return jsonify(error='unknown playback type')
+    return jsonify(error='unknown playback type')
+
+@app.route('/btc')
+def btc():
+    apiURL = "https://btc-e.com/api/2/btc_usd/ticker"
+    s = urlopen(apiURL)#, eparams)
+    r = s.read()
+    r = r.strip()
+    jsondata = r
+    data = json.loads(jsondata)
+    return jsonify(btc = data['ticker']['last'],
+                   high = data['ticker']['high'],
+                   low = data['ticker']['low'])
+    
+@app.route('/mpd/play')
+def play():
+    client = MPDClient()
+    client.timeout = 10
+    client.idletimeout = None
+    client.connect("mpd.shack", 6600)
+    client.play()
+    client.close()
+    client.disconnect()
+    return jsonify(status='success', action='play')
+
+@app.route('/mpd/pause')
+def pause():
+    client = MPDClient()
+    client.timeout = 10
+    client.idletimeout = None
+    client.connect("mpd.shack", 6600)
+    client.pause()
+    client.close()
+    client.disconnect()
+    return jsonify(status='success', action='pause')
     
 @app.route('/say/', methods=['POST'])
 def say():
     if request.headers['Content-Type'] == 'application/json':
-        data = simplejson.loads(request.data)
+        data = json.loads(request.data.decode())
         text = data['text']
         if len(text) <= 300:
-            print("ohai")
-            print(os.getcwd())
-            os.system(os.getcwd() + '/gobbelz/naturalvoices.att ' + text + " &")
+            print(text)
+            args = ['espeak', '-vde']
+            args.append(text)
+            p = subprocess.check_call(args)
         else:
             return(jsonify(
                 status='error',
@@ -29,7 +95,18 @@ def say():
         return(jsonify(
             status='success',
             text=data['text']))
-
+    elif request.headers['Content-Type'] == 'text/plain':
+        text = request.data.decode('utf-8')
+        if len(text) <= 300:
+            print(text)
+            args = ['espeak', '-vde']
+            args.append(text)
+            p = subprocess.check_call(args)
+        else:
+            return "error: Your text exceeded the limit of 300 chars"
+        
+        return "success: {}".format(text)
+	
     else:
         return(jsonify(
             status='error',
@@ -37,4 +114,4 @@ def say():
         
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8081, debug=True)
